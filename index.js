b53e8f43bec9af0ad3350cf6b75b5386ef679256
zougataga <107312482+zougataga@users.noreply.github.com>
2022-12-03 20:05:12 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// llx404 on git
-require("./src/server/index.js");
-
-process.on("unhandledRejection", (reason, promise) => { return console.log(reason, promise) });
-process.on("uncaughtException", (err) => { return console.log(err) });
-process.on('uncaughtExceptionMonitor', (err, origin) => { return console.log(err, origin); });
-process.on('multipleResolves', (type, promise, reason) => { return console.log(type, promise, reason) });
-module.exports = __dirname;
-
